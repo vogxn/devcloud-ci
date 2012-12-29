33bdbbd23402862c565830979318955f8c449280
Prasanna Santhanam <tsp@apache.org>
2012-12-28 23:18:00 -0800

    missed passing to the run()

--- ci/devcloud/setUpTestWorker.py
+++ ci/devcloud/setUpTestWorker.py
@@ -158,5 +158,5 @@ if __name__ == '__main__':
     args = parser.parse_args()
     reporter = DevCloudReporter(args.host, args.passwd, args.user, args.out)
     reporter.postNetworkInfo()
-    resultXml = run(TestWorker())
+    resultXml = run(TestWorker(), marvin)
     reporter.copyFile(resultXml)
